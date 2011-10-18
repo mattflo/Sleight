4d7fd8197a648deb4b07d777d33276de1ddf3632
Amir Rajan <ar@amirrajan.net>
2011-10-18 18:43:59 -0500

    Initial commit for Sleight

---
+++ Sleight.Tests/describe_Mock.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NSpec;
+
+namespace Sleight.Tests
+{
+    class describe_Mock : nspec
+    {
+        Mock mock;
+
+        dynamic asType;
+
+        object result;
+
+        void before_each()
+        {
+            mock = new Mock();
+
+            asType = mock;
+        }
+
+        void calling_methods()
+        {
+            context["stubbing at the method level with"] = () =>
+            {
+                act = () => result = asType.SayHello("Jane Doe");
+
+                context["method has not been stubbed out"] = () =>
+                {
+                    it["returns null"] = () =>
+                        result.should_be(null);
+                };
+
+                context["method has been stubbed"] = () =>
+                {
+                    before = () => mock.Stub("SayHello").Returns("Hello");
+
+                    it["returns stubbed value"] = () =>
+                        result.should_be("Hello");
+
+                    context["subsequent stub of method"] = () =>
+                    {
+                        before = () => mock.Stub("SayHello").Returns("Hi");
+
+                        it["redefines method to return new value"] = () =>
+                            result.should_be("Hi");
+                    };
+                };
+            };
+
+            context["stubbing at the parameter level"] = () =>
+            {
+                before = () =>
+                {
+                    mock.Stub("SayHello").Returns("Hello");
+
+                    mock.Stub("SayHello").WithParameters("John Doe").Returns("Hello John");
+
+                    mock.Stub("SayHello").WithParameters("Jan Doe").Returns("Hello Jane");
+                };
+
+                xit["returns the value based on parameter granularity"] = () =>
+                {
+                    (asType.SayHello("John Doe") as object).should_be("Hello John");
+
+                    (asType.SayHello("Jane Doe") as object).should_be("Hello Jane");
+
+                    (asType.SayHello("Anything Else") as object).should_be("Hello");
+                };
+            };
+        }
+
+        void recording_method_calls()
+        {
+            it["return null for last execution if method was never called"] = () =>
+                    mock.ExecutionFor("SayHi").should_be(null);
+
+            context["method called with one parameter"] = () => 
+            {
+                act = () => asType.SayHello("Jane Doe");
+
+                it["records parameters used with method"] = () =>
+                    mock.ExecutionFor("SayHello").Parameter.should_be("Jane Doe");
+
+                context["method is stubbed"] = () =>
+                {
+                    before = () => mock.Stub("SayHello").Returns("Hello");
+
+                    it["records value returned"] = () =>
+                        mock.ExecutionFor("SayHello").ReturnValue.should_be("Hello");
+                };
+
+                context["subsquent call to method is preformed"] = () =>
+                {
+                    act = () => asType.SayHello("John Doe");
+
+                    it["records the last call"] = () =>
+                        mock.ExecutionFor("SayHello").Parameter.should_be("John Doe");
+                };
+            };
+
+            context["method called with no parameters"] = () =>
+            {
+                act = () => asType.SayHello();
+
+                it["recorded parameter is null"] = () =>
+                    mock.ExecutionFor("SayHello").Parameter.should_be(null);
+            };
+
+            context["method called with multiple parameters"] = () =>
+            {
+                act = () => asType.SayHello("Jane", "Doe");
+
+                it["recorded parameter is the first parameter"] = () =>
+                    mock.ExecutionFor("SayHello").Parameter.should_be("Jane");
+
+                it["recorded parameters has each parameter that was used"] = () =>
+                {
+                    mock.ExecutionFor("SayHello").Parameters[0].should_be("Jane");
+
+                    mock.ExecutionFor("SayHello").Parameters[1].should_be("Doe");
+                };
+            };
+
+            context["method is called with type arguments"] = () =>
+            {
+                act = () => asType.SayHello<string>("Jane");
+
+                it["records type argument"] = () => 
+                    mock.ExecutionFor("SayHello").TypeArgument.should_be(typeof(string));
+            };
+
+            context["method is called with multiple type arguments"] = () =>
+            {
+                act = () => asType.SayHello<string, int>("Jane");
+
+                it["records type arguments"] = () =>
+                {
+                    mock.ExecutionFor("SayHello").TypeArguments[0].should_be(typeof(string));
+
+                    mock.ExecutionFor("SayHello").TypeArguments[1].should_be(typeof(int));
+                };
+            };
+        }
+    }
+}
---
+++ Sleight/Mock.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace Sleight
+{
+    public class Mock : DynamicObject
+    {
+        string currentMember;
+
+        Dictionary<string, object> values;
+
+        Dictionary<string, Execution> lastExecution;
+
+        public Mock()
+        {
+            values = new Dictionary<string, object>();
+
+            lastExecution = new Dictionary<string, Execution>();
+        }
+
+        public Mock WithParameters(string value)
+        {
+            return this;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            result = values.ValueOrNull(binder.Name);
+
+            RecordExecution(binder.Name, args, result, TypeArguments(binder));
+
+            return true;
+        }
+
+        void RecordExecution(string name, object[] args, object returnValue, Type[] typeArguments)
+        {
+            lastExecution[name] = new Execution
+            {
+                Parameter = args.FirstOrDefault(),
+                Parameters = args,
+                ReturnValue = returnValue,
+                TypeArgument = typeArguments.FirstOrDefault(),
+                TypeArguments = typeArguments
+            };
+        }
+
+        public Mock Stub(string member)
+        {
+            currentMember = member;
+
+            values[member] = null;
+
+            return this;
+        }
+
+        public Execution ExecutionFor(string member)
+        {
+            return lastExecution.ValueOrNull(member);
+        }
+
+        public void Returns(object value)
+        {
+            values[currentMember] = value;
+        }
+
+        private Type[] TypeArguments(InvokeMemberBinder binder)
+        {
+            var csharpBinder = binder.GetType().GetInterface("Microsoft.CSharp.RuntimeBinder.ICSharpInvokeOrInvokeMemberBinder");
+
+            return (csharpBinder.GetProperty("TypeArguments").GetValue(binder, null) as IList<Type>).ToArray();
+        }
+    }
+}
